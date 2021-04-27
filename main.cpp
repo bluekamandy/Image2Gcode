9c16c59cce96b2d85851ad925f99324209436749
Masood Kamandy <masood@masoodkamandy.com>
2021-04-27 08:57:01 -0700

    Base code and CMake set up.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
-// A simple program that computes the square root of a number
 #include <cmath>
 #include <cstdlib>
 #include <iostream>
 #include <fstream> // Read/write files
 #include <string>
+#include "config.h"
 
 #define LOG(msg) \
     std::cout << msg << std::endl
@@ -13,16 +13,9 @@ int main(int argc, char *argv[])
     if (argc < 2)
     {
         LOG("hello world");
-        std::cout << "Usage: " << argv[0] << " number" << std::endl;
+        std::cout << argv[0] << " Version " << IMGTOGCODE_VERSION_MAJOR << "." << IMGTOGCODE_VERSION_MINOR << '\n';
         return 1;
     }
 
-    // convert input to double
-    const double inputValue = atof(argv[1]);
-
-    // calculate square root
-    const double outputValue = sqrt(inputValue);
-    std::cout << "The square root of " << inputValue << " is " << outputValue
-              << std::endl;
     return 0;
 }
